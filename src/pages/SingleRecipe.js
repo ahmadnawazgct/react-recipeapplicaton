4f5d54ecebf7b75af1cafb9e4e95705798eb1899
Nawaz Ahmad <ahmadnawazgct@gmail.com>
2019-05-13 18:34:55 -0700

    2nd commit

---
+++ src/pages/SingleRecipe.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class SingleRecipe extends Component{
+    render(){
+        return(
+            <h4>Hellof From Single Recipe page</h4>
+        )
+    }
+}
