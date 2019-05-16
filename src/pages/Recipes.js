9d67b02a2f28a83e7b40f625e20b3d5fe4d8ef6b
Nawaz Ahmad <ahmadnawazgct@gmail.com>
2019-05-15 17:33:10 -0700

    environment variable change

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -3,33 +3,55 @@ import Search from '../components/Search';
 import RecipeList from '../components/RecipeList';
 import { recipeData } from '../data/tempList';
 
-export default class Recipes extends Component{
-    constructor(props){
-        super(props)
-    }
-    state={
-        recipes:recipeData,
-        search:""
-    }
-    hangelChange=e=>{
-        console.log("change called");
-        this.setState({
-        search: e.target.value
-        });
-    }
-    handleSumbit=e=>{
-        e.preventDefault();
-    }
-    render(){
-        return(
-            <>
-                <Search  
-                    search={this.state.search}
-                    handleChange={this.hangelChange}
-                    handleSumbit={this.handleSumbit}
-                />
-                <RecipeList recipes={this.state.recipes} />
-            </>
-        )
-    }
-}
+export default class Recipes extends Component {
+                 constructor(props) {
+                super(props);
+                this.getRecipe = this.getRecipe.bind(this);
+                }
+                 state = {
+                   recipes:recipeData,
+                   search: "",
+                   url: `https://www.food2fork.com/api/search?key=${process.env.REACT_APP_API_KEY}`
+                 };
+                async getRecipe(){
+                    try{
+                        const data= await fetch(this.state.url);
+                        console.log(data);
+                        const jsonData = await data.json();
+                        console.log(jsonData);
+                        this.setState({
+                            recipes:jsonData.recipes
+                        })
+                    } 
+                    catch(error){
+                        console.log(error);
+                        
+                    }
+                }
+                componentDidMount(){
+                    this.getRecipe();
+                }
+                 hangelChange = e => {
+                   console.log("change called");
+                   this.setState({
+                     search: e.target.value
+                   });
+                 };
+                 handleSumbit = e => {
+                   e.preventDefault();
+                 };
+                 render() {
+                   return (
+                     <>
+                       <Search
+                         search={this.state.search}
+                         handleChange={this.hangelChange}
+                         handleSumbit={this.handleSumbit}
+                       />
+                       <RecipeList
+                         recipes={this.state.recipes}
+                       />
+                     </>
+                   );
+                 }
+               }
